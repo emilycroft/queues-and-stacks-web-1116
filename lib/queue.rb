e11e8c041e6dcccb35d756c94ab3fe8d596f2a9c
emilycroft <emilyhcroft@gmail.com>
2017-03-25 15:52:06 -0400

    Done.

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,17 @@
 require_relative 'stack'
 
 class Queue
+  attr_accessor :queue
 
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(arg)
+    @queue << arg
+  end
+
+  def dequeue
+    @queue.shift
+  end
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,19 @@
 class Stack
+  attr_accessor :stack
 
+  def initialize
+    @stack = []
+  end
+
+  def push(obj)
+    @stack << obj
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def size
+    @stack.length
+  end
 end
